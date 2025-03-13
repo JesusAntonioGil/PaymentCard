317b9657965dc70b1bfe272a27e43724f4787ef4
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-03-13 12:39:01 +0100

    Create project

---
+++ PaymentCard/PaymentCard/PaymentCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentCardApp.swift
+//  PaymentCard
+//
+//  Created by Jesus Antonio Gil on 13/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PaymentCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
